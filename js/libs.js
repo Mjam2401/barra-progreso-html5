3f0b639be33f2ee7b52d90d5874c4f424ffcdf43
Pedro Ventura <pedrojventura@gmail.com>
2012-10-26 17:23:17 +0300

    Update js/libs.js

--- js/libs.js
+++ js/libs.js
@@ -1,8 +1,8 @@
-#progreso actual
+//progreso actual
 var currProgress = 0;
-#esta la tarea completa
+//esta la tarea completa
 var done = false;
-#cantidad total de progreso
+//cantidad total de progreso
 var total = 100;
 
 /**
@@ -11,26 +11,26 @@ var total = 100;
  * @version 1.0
  */
 function startProgress() {
-	#recuperamos el elemento de progreso
+	//recuperamos el elemento de progreso
 	var prBar = document.getElementById("prog");
-	#get the start button
+	//get the start button
 	var startButt = document.getElementById("startBtn");
-	#recuperamos el valor del texto
+	//recuperamos el valor del texto
 	var val = document.getElementById("numValue");
-	#deshabilitamos el botón
+	//deshabilitamos el botón
 	startButt.disabled = true;
-	#actualiza la barra de progreso
+	//actualiza la barra de progreso
 	prBar.value = currProgress;
-	#actualizamos el indicador visual con el texto
+	//actualizamos el indicador visual con el texto
 	val.innerHTML = Math.round((currProgress/total)*100)+"%";
-	#incrementamos el valor del progreso cada vez que la función se ejecuta
+	//incrementamos el valor del progreso cada vez que la función se ejecuta
 	currProgress++;
-	#comprobamos si hemos terminado
+	//comprobamos si hemos terminado
 	if(currProgress>100) done = true;
-	# sino hemos terminado, volvemos a llamar a la función después de un tiempo
+	// sino hemos terminado, volvemos a llamar a la función después de un tiempo
 	if(!done) {
 	 	setTimeout("startProgress()", 100);
-		#tarea terminada, habilitar el botón y resetear variables
+		//tarea terminada, habilitar el botón y resetear variables
 	} else {
 	  document.getElementById("startBtn").disabled = false;
 	  done = false;
